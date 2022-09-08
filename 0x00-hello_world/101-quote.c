2214820237238aa5e6df21571ae26342aa7eb34e
Bonke Sam <bonkesam9@gmail.com>
2022-09-08 16:30:57 -0700

    Out put to stder

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints to stderr
+ *
+ * Description: Prints to the standard error
+ * Return: Returns 0 always
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that pece of art is useful\"
+			- Dora Korpra, 2015-10-19\n");
+	return (1);
+}

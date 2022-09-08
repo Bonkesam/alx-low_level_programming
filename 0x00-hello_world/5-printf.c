c195508f439d3482e39224ec7d39e93ddcb77bdd
Bonke Sam <bonkesam9@gmail.com>
2022-09-08 15:37:07 -0700

    Prints using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Uses printf()
+ *
+ * Description: This program prints only one line using the printf() function
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0);
+}

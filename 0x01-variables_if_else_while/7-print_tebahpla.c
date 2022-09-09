5a0c412aa2d81e6c61e004821a260ab6a441be85
Bonke Sam <bonkesam9@gmail.com>
2022-09-09 10:41:00 -0700

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints in reverse
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i > 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

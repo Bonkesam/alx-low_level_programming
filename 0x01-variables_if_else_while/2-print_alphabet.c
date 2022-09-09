a09704b9c76a06d0e554c317e06dd176c2a9fc00
Bonke Sam <bonkesam9@gmail.com>
2022-09-09 07:44:03 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Alphebet
+ *
+ * Description: Prints all lower case alphabet
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a', i = < 'z', i++)
+	{
+		putchar("%c\n", i);
+	}
+	return (0);
+}

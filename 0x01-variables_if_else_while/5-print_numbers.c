3a556fd5291a70af01b329ec250b2b76c6bb91a7
Bonke Sam <bonkesam9@gmail.com>
2022-09-09 08:31:19 -0700

    Prints numbers less than 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - decimal numbers
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i >= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

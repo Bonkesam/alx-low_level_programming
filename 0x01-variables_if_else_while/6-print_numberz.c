ee63f3758db377569f4b98540350769b3ff3113b
Bonke Sam <bonkesam9@gmail.com>
2022-09-09 10:23:47 -0700

    Print all numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base ten figures
+ *
+ * Return: Always return zero (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

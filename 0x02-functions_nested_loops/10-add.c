54081fdcf2fd452d938eb8d132ad636232ddfb70
Bonke Sam <bonkesam9@gmail.com>
2022-09-14 06:28:54 -0700

    Adds 2 intagers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @i: the first intager to add
+ * @y: the second param to add
+ *
+ * description: adds 2 intagers
+ * Return: Always return 0 (success)
+ */
+
+int add(int i, int y)
+{
+
+	int z;
+
+	z = i + y;
+
+	return (z);
+}

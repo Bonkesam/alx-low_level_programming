2e0ab893729795f8a72cb979a1f247b46a4f50e2
Bonke Sam <bonkesam9@gmail.com>
2022-09-14 04:26:10 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - Checks absolute
+ * @i: takes an intager
+ *
+ * Description: makes a number absolute and returns its absolute value
+ * Return: Always return 0 (success)
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = (i * -1);
+	}
+	else
+	{
+		i = i;
+	}
+
+	return (0);
+}

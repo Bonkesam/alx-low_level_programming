bf31119f563d0b222ea28a529bc197073dc955d1
Bonke Sam <bonkesam9@gmail.com>
2022-09-14 04:32:43 -0700

    Prints last figure

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @i: the intager under test
+ *
+ * description: prints the last digit of a number
+ * Return: Always return 0 (success)
+ */
+
+int print_last_digit(int i)
+{
+	int l;
+
+	l = i % 10;
+	return (l);
+}

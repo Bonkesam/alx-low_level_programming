1dcac150e4d1f0ae33087b942f10324a95f30fc9
Bonke Sam <bonkesam9@gmail.com>
2022-09-15 07:10:32 -0700

    Avoids infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ *
+ * Description: The code solves an infinite loop
+ * Return: Always return a 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming: (\n");
+
+	i = 0;
+
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}

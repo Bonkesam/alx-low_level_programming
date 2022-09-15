bbc0b044363bbfa34cec6e2d2f90cbe237eba90a
Bonke Sam <bonkesam9@gmail.com>
2022-09-15 09:17:26 -0700

    Tests the sign of numbers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,6 @@
 /**
  * main - tests function that prints if integer is positive or negative
  *
- * Description: This code tests the sign of numbers
  * Return: 0
  */
 
@@ -12,6 +11,7 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);

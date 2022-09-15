c8538b9a092449b80f04c30856f6962b748cf077
Bonke Sam <bonkesam9@gmail.com>
2022-09-15 07:15:08 -0700

    corrects spelling

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming: (\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
@@ -22,7 +22,7 @@ int main(void)
 	*}
 	*/
 
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }

772a2577a9f3859da8198224334247618f2afb9f
Bonke Sam <bonkesam9@gmail.com>
2022-09-19 09:37:06 -0700

    Prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: String to print
+ *
+ * Description: Prints a string
+ * Return: Always a 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}

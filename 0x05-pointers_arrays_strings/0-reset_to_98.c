b379687b284ce9a147db71fe78bc451a6e0ceb79
Bonke Sam <bonkesam9@gmail.com>
2022-09-19 09:03:59 -0700

    Changes pointer to interger

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates value of int
+ * @n: int to update
+ *
+ * Description: Takes a parameter pointer and chan
+ * ges to int
+ * Return: Always return 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

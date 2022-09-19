811c2a191a6cd8fc2b6a3e8bec5615a7becc1b03
Bonke Sam <bonkesam9@gmail.com>
2022-09-19 09:47:31 -0700

    void print_rev(char *s)

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * print_rev - Prints in reverse
+ * @s: the string
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(i);
+
+	while (i--)
+	{
+		putchar(*(s + i));
+	}
+	putchar(10);
+}

0cf267c92c3ffff75a53c66453fe72f69d990191
Bonke Sam <bonkesam9@gmail.com>
2022-09-19 09:11:27 -0700

    Swaps values of intagers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps 2 intagers
+ * @a: first int
+ * @b:second int
+ *
+ * Return: Always return 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*b = *a;
+	*a = *b;
+}

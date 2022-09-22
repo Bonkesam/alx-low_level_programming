948836a9a09eef9d04b815ece42c43f6e36d4ab4
Bonke Sam <bonkesam9@gmail.com>
2022-09-22 03:12:33 -0700

    Capiytalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -30,7 +30,7 @@ char *cap_string(char *str)
 
 	while (str[++index])
 	{
-		while (!(str[index] >= 'a') && (!(str[index] >= 'z')))
+		while (!(str[index] >= 'a') && ((str[index] >= 'z')))
 		{
 			index++;
 		}

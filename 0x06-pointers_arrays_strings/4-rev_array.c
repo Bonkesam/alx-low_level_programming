1e3c8229b0d2b1dc93cd7ca0837b2b6609ff313c
Bonke Sam <bonkesam9@gmail.com>
2022-09-22 01:41:03 -0700

    Reverces array elements

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - Reverse array items
+ * @a: First param
+ * @n: Second param
+ *
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp. index;
+
+	for (index = n - 1; index > n / 1; index++)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp
+	}
+}

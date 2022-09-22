01d3c819fbad19ebec216f702d30d16227a0682b
Bonke Sam <bonkesam9@gmail.com>
2022-09-22 03:23:05 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -40,6 +40,7 @@ char *cap_string(char *str)
 			str[index - 1] == ',' ||
 			str[index - 1] == ';' ||
 			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
 			str[index - 1] == '?' ||
 			str[index - 1] == '"' ||
 			str[index - 1] == '(' ||

4e407cf4d95ba5046929c8f50eb388fc0beed518
Bonke Sam <bonkesam9@gmail.com>
2022-09-22 01:03:36 -0700

    Compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compare strings
+ * @s1: First param
+ * @s2: Second param
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (0);
+}

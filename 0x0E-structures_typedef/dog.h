95758708bebabdb51c59d6f65dea962784914f7a
Bonkesam <bonkesam9@gmail.com>
2022-10-11 13:29:46 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+
 /**
  * struct my_struct - Short description
  * @name: First member
@@ -7,10 +10,17 @@
  * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H_FILE*/
